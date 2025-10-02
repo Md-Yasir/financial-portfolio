babb2ab7b35300753ef177e5c2583ee876394a61
md-yasir <mohdyasir231990@gmail.com>
2025-10-02 11:22:22 +0530

    New SpringBoot app

---
+++ src/main/java/com/yass/fin/portfolio/FinancialPortfolioTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.yass.fin.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialPortfolioTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialPortfolioTrackerApplication.class, args);
+	}
+
+}
